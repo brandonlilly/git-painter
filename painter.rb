60eaea218f60526d92a9455745fbc65f114da313
Brandon Lilly <brandonjlilly@gmail.com>
2015-05-14 17:46:13 -0700

    cleanup

--- painter.rb
+++ painter.rb
@@ -1,6 +1,6 @@
 require 'git'
 
-str = <<-IMG.split("\n").map {|row| row.split('')}
+ = <<-IMG.split("\n").map {|row| row.split('')}
 XXABXXXXXBXABXXXXABXXXXABXXABXXXXXXXXXXXXXXXXXXXXXXX
 XXABXXXXABXABXXXXABXXXXABXXABXXXXXXXXXXXXXXXXXXXXXXX
 XXABXXXXABXABXXXXABXXXXXABBBXXXXXXXXXXXXXXXXXXXXXXXX
@@ -9,15 +9,14 @@ XXABXXXXABXABXXXXABXXXXXXABXXXXXXXXXXXXXXXXXXXXXXXXX
 XXABBBBXABXABBBBXABBBBXXXABXXXXXXXXXXXXXXXXXXXXXXXXX
 XXXAAAXXXBXXAAAXXXAAAXXXXABXXXXXXXXXXXXXXXXXXXXXXXXX
 IMG
+
 days = 24*60*60
 monday = Time.now - 52*7*days
 monday += monday.monday? ? 0 : (7 - monday.wday) * days
-p monday
-p monday.wday
 
 A = []
 B = []
-str.each_with_index do |row, wday|
+.each_with_index do |row, wday|
   row.each_with_index do |char, weeks|
     if char == 'A'
       A << monday + (wday + weeks*7) * days
@@ -26,4 +25,3 @@ str.each_with_index do |row, wday|
     end
   end
 end
-p A
